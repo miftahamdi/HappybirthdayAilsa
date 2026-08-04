4e5c9079585bdd6fd9594cb563ff4fdbf9fc53b3
miftahamdi <112234119+miftahamdi@users.noreply.github.com>
2026-08-04 23:09:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const text =
+"Semoga hari ini menjadi hari yang paling indah untukmu yaw ❤️";
+
+let i = 0;
+
+function typeWriter(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typeWriter,50);
+
+}
+
+}
+
+typeWriter();
+
+document.getElementById("btn").onclick=function(){
+
+document.getElementById("hidden").style.display="block";
+
+this.style.display="none";
+
+}
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.className="heart";
+
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.animationDuration=(Math.random()*3+3)+"s";
+
+heart.style.fontSize=(20+Math.random()*30)+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+}
+
+setInterval(createHeart,250);
